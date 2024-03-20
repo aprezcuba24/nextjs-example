0d2c17a356901b29c5fd42f82edbdd3c7e158343
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2024-03-20 20:42:59 +0000

    Add BtnDialogForm

--- src/app/categories/DialogForm.tsx
+++ src/app/categories/DialogForm.tsx
@@ -1,35 +1,6 @@
-'use client'
-
-import Dialog from "@/components/Dialog";
 import FormCategory from "./form";
-import React, { useCallback, useState } from "react";
-import { Button } from "@/components/ui/button";
+import { BtnDialogForm, BtnDialogFormProps } from "@/components/BtnDialogForm";
 
-type DialogFormProps = {
-  action: (object: any) => Promise<any>,
-  defaultValues: any,
-  title: string,
-  btnIcon?: React.ReactNode,
-  btnText?: string,
-}
-
-export function DialogForm({ action, title, btnIcon, btnText, defaultValues }: DialogFormProps) {
-  btnText = btnText || title
-  const [open, setOpen] = useState(false)
-  const handleAction = useCallback(async (props: any) => {
-    await action(props);
-    setOpen(false)
-  }, [action]);
-  const handleOpen = useCallback(() => {
-    setOpen(prev => !prev)
-  }, []);
-
-  return (
-    <>
-      <Button variant="outline" onClick={handleOpen} size={btnIcon ? "icon" : "default"}>{btnIcon || btnText}</Button>
-      <Dialog title={title} open={open} onOpenChange={handleOpen}>
-        <FormCategory action={handleAction} defaultValues={defaultValues} />
-      </Dialog>
-    </>
-  )
+export function DialogForm({ ...props }: Omit<BtnDialogFormProps, 'Component'>) {
+  return <BtnDialogForm Component={FormCategory} {...props} />
 }
---
+++ src/components/BtnDialogForm.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import Dialog from "@/components/Dialog";
+import React, { useCallback, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+type Form = React.ComponentType<{
+  action: any,
+  defaultValues: any,
+}>
+
+export type BtnDialogFormProps = {
+  action: (object: any) => Promise<any>,
+  defaultValues: any,
+  title: string,
+  btnIcon?: React.ReactNode,
+  btnText?: string,
+  Component: Form
+}
+
+export function BtnDialogForm({ action, title, btnIcon, btnText, Component, defaultValues }: BtnDialogFormProps) {
+  btnText = btnText || title
+  const [open, setOpen] = useState(false)
+  const handleAction = useCallback(async (props: any) => {
+    await action(props);
+    setOpen(false)
+  }, [action]);
+  const handleOpen = useCallback(() => {
+    setOpen(prev => !prev)
+  }, []);
+
+  return (
+    <>
+      <Button variant="outline" onClick={handleOpen} size={btnIcon ? "icon" : "default"}>{btnIcon || btnText}</Button>
+      <Dialog title={title} open={open} onOpenChange={handleOpen}>
+        <Component action={handleAction} defaultValues={defaultValues} />
+      </Dialog>
+    </>
+  )
+}
